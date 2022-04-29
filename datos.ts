16b7c3ef33c8ea77ec7bde597f67cd5cabd0ba8a
CintiaAlvarez <104107300+CintiaAlvarez@users.noreply.github.com>
2022-04-29 04:43:54 -0300

    Initial commit

---
+++ datos.ts
@@ -0,0 +1,6 @@
+let paso1: string = "este es mi paso 1";
+let paso2: string = "este es mi paso 2";
+let titulo: string = "como hacer una torta";
+console.log(titulo);
+console.log(paso1);
+console.log(paso2);
